47fcfedd01da2a4b1730187978f93fcdb76d5dcb
javets70 <javetsm70@gmail.com>
2026-05-04 23:38:11 +0530

    improving index

--- src/log.rs
+++ src/log.rs
@@ -2,8 +2,12 @@
 use std::fs::{File, OpenOptions};
 use std::io::{BufWriter, Write};
 
+// DEFAULTS TO
+// BATCH SIZE 10
 pub struct LogWriter {
     writer: BufWriter<File>,
+    batch_count: usize,
+    batch_size: usize,
 }
 
 impl LogWriter {
@@ -11,7 +15,13 @@ impl LogWriter {
         let file = OpenOptions::new().append(true).create(true).open(path)?;
         Ok(LogWriter {
             writer: BufWriter::new(file),
+            batch_count: 0,
+            batch_size: 10,
         })
+    }
+
+    pub fn set_batch_size(&mut self, batch_size: usize) {
+        self.batch_size = batch_size;
     }
 
     pub fn write_entry(&mut self, key: &[u8], value: &[u8]) -> std::io::Result<()> {
@@ -20,6 +30,17 @@ impl LogWriter {
         self.writer.write_all(key)?;
         self.writer.write_all(value)?;
 
+        self.batch_count += 1;
+        self.maybe_sync()?;
+
+        Ok(())
+    }
+
+    pub fn maybe_sync(&mut self) -> std::io::Result<()> {
+        if self.batch_count >= self.batch_size && self.batch_size > 0 {
+            self.batch_count = 0;
+            self.flush_and_sync()?;
+        }
         Ok(())
     }
 
--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,29 @@ use std::fs::{File, OpenOptions};
 use std::io::{self, stdin, BufRead, BufReader, ErrorKind, Read, Write};
 
 pub mod log;
+// pub mod store;
+
+// HashMap<Key bytes , (value offset , value length)>
+struct Index(HashMap<Vec<u8>, (u64, usize)>);
+
+impl Index {
+    pub fn new() -> Self {
+        Index(HashMap::new())
+    }
+    pub fn insert(&mut self, key: Vec<u8>, value_offset: u64, value_len: usize) {
+        self.insert(key, value_offset, value_len);
+    }
+}
 
 fn main() -> std::io::Result<()> {
     let log_path = "kestrel.log";
     let mut store = HashMap::new();
 
-    OpenOptions::new().create(true).write(true).open(log_path)?;
-    let mut file = OpenOptions::new().read(true).open(log_path)?;
+    let mut file = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .read(true)
+        .open(log_path)?;
     loop {
         let key_len = match read_u32(&mut file) {
             Ok(n) => n as usize,
@@ -31,10 +47,20 @@ fn main() -> std::io::Result<()> {
             Err(e) => return Err(e),
         };
 
-        let mut key = vec![0u8; key_len];
-        file.read_exact(&mut key)?;
-        let mut value = vec![0u8; val_len];
-        file.read_exact(&mut value)?;
+        let key = match read_bytes(&mut file, key_len)? {
+            Some(data) => data,
+            None => {
+                eprintln!("Incomplete or partial record entry in log; discarding");
+                break;
+            }
+        };
+        let value = match read_bytes(&mut file, val_len)? {
+            Some(data) => data,
+            None => {
+                eprintln!("Incomplete or partial record entry in log; discarding");
+                break;
+            }
+        };
 
         let key_str = String::from_utf8_lossy(&key).into_owned();
         let val_str = String::from_utf8_lossy(&value).into_owned();
@@ -42,6 +68,7 @@ fn main() -> std::io::Result<()> {
     }
 
     let mut log_writer = LogWriter::new(log_path)?;
+    log_writer.set_batch_size(1000);
     loop {
         let mut input = String::new();
         stdin().read_line(&mut input)?;
@@ -60,14 +87,8 @@ fn main() -> std::io::Result<()> {
                 // update store
                 store.insert(key.clone(), value.clone());
 
-                // let mut file = OpenOptions::new()
-                //     .append(true)
-                //     .create(true)
-                //     .open(log_path)?;
-                // write_entry(&mut file, &key.into_bytes(), &value.into_bytes())?;
-                // file.sync_all()?;
                 log_writer.write_entry(&key.into_bytes(), &value.into_bytes())?;
-                log_writer.flush_and_sync()?;
+                log_writer.maybe_sync()?;
             }
             Some("GET") => {
                 let key = parts.next().unwrap_or_default().to_string();
@@ -77,25 +98,28 @@ fn main() -> std::io::Result<()> {
                 }
             }
 
-            Some("QUIT") => break,
+            Some("QUIT") => {
+                log_writer.flush_and_sync()?;
+                break;
+            }
             _ => eprintln!("Unknown Command. To exit type QUIT and enter."),
         }
     }
     Ok(())
 }
 
-// fn write_entry(file: &mut std::fs::File, key: &[u8], value: &[u8]) -> std::io::Result<()> {
-//     file.write_all(&(key.len() as u32).to_be_bytes())?;
-//     file.write_all(&(value.len() as u32).to_be_bytes())?;
-//     file.write_all(key)?;
-//     file.write_all(value)?;
-
-//     Ok(())
-// }
-
 fn read_u32(reader: &mut impl Read) -> std::io::Result<u32> {
     let mut buf = [0u8; 4];
     reader.read_exact(&mut buf)?;
 
     Ok(u32::from_be_bytes(buf))
+}
+
+fn read_bytes(reader: &mut impl Read, len: usize) -> std::io::Result<Option<Vec<u8>>> {
+    let mut buf = vec![0u8; len];
+    match reader.read_exact(&mut buf) {
+        Ok(_) => Ok(Some(buf)),
+        Err(e) if e.kind() == ErrorKind::UnexpectedEof => Ok(None),
+        Err(e) => Err(e),
+    }
 }
