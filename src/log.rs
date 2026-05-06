f8d494677a2ad4cba2d9e10a1bb233b85e6fe5ae
javets70 <javetsm70@gmail.com>
2026-05-06 14:03:41 +0530

    segmentation

--- src/log.rs
+++ src/log.rs
@@ -1,14 +1,18 @@
 // log.rs
+use crate::get_log_path;
 use std::fs::{File, OpenOptions};
 use std::io::{BufWriter, Seek, Write};
 
 // DEFAULTS TO
 // BATCH SIZE 10
+//
 pub struct LogWriter {
     writer: BufWriter<File>,
     batch_count: usize,
     batch_size: usize,
     current_offset: u64,
+    // MAX LENGTH OF A LOG FILE IN BYTES
+    max_segment_size: u64,
 }
 
 // NOW WE ARE STORING VALUE_OFFSET AND VALUE_LENGTH
@@ -24,6 +28,7 @@ impl LogWriter {
             batch_count: 0,
             batch_size: 10,
             current_offset,
+            max_segment_size: 50,
         })
     }
 
@@ -31,24 +36,63 @@ impl LogWriter {
         self.batch_size = batch_size;
     }
 
-    pub fn write_entry(&mut self, key: &[u8], value: &[u8]) -> std::io::Result<(u64, usize)> {
+    pub fn set_segment_size(&mut self, value: u64) {
+        self.max_segment_size = value;
+    }
+
+    pub fn update_writer(&mut self, current_segment_number: &u32) -> std::io::Result<()> {
+        let new_log_path = get_log_path(&(current_segment_number + 1u32));
+        let file = OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open(new_log_path)?;
+        let current_offset = file.metadata()?.len();
+        self.writer = BufWriter::new(file);
+        self.current_offset = current_offset;
+
+        Ok(())
+    }
+
+    pub fn write_entry(
+        &mut self,
+        key: &[u8],
+        value: &[u8],
+        current_segment_number: &u32,
+    ) -> std::io::Result<(u64, usize, bool)> {
         // 4 BYTES FOR KEY LENGTH (u32)
         // 4 BYTES FOR 4 VALUE LENGTH (u32)
-        // REST FOR ACTUAL KEY (SIZE COMPUTED AT EXECUTION)
+        // ACTUAL KEY LENGTH (SIZE COMPUTED AT EXECUTION)
+        // ACTUAL VALUE LENGTH (SIZE COMPUTED AT EXECUTION)
+        let data_length = 4 + 4 + (key.len() as u64) + (value.len() as u64);
+
+        // CURRENT STRATEGY FOR MAX_SEGMENT_SIZE IS
+        // WE CHECK IF THE CURRENT WRITE WILL EXCEED THE
+        // MAX_SEGMENT_SIZE , IF IT DOES THEN WE INCREMENT
+        // SEGMENT_ID IN INDEX AND MOVE ONTO NEXT FILE
+
+        let mut writer_updated = false;
+        if data_length + self.current_offset > self.max_segment_size {
+            // flush old writer
+            self.flush_and_sync()?;
+            writer_updated = true;
+            self.update_writer(current_segment_number)?;
+        }
+
+        // 4 BYTES FOR KEY LENGTH (u32)
+        // 4 BYTES FOR 4 VALUE LENGTH (u32)
+        // REST FOR ACTUAL LENGTH (SIZE COMPUTED AT EXECUTION)
         let value_offset = self.current_offset + 4 + 4 + (key.len() as u64);
         self.writer.write_all(&(key.len() as u32).to_be_bytes())?;
         self.writer.write_all(&(value.len() as u32).to_be_bytes())?;
         self.writer.write_all(key)?;
         self.writer.write_all(value)?;
 
-        // VALUE OFFSET ALREADY ACCOUNTS FOR
-        // ALL THE BYTES EXCEPT ACTUAL VALUE
-        self.current_offset += value_offset + (value.len() as u64);
+        self.current_offset += data_length;
 
         self.batch_count += 1;
         self.maybe_sync()?;
 
-        Ok((value_offset, value.len()))
+        Ok((value_offset, value.len(), writer_updated))
     }
 
     pub fn maybe_sync(&mut self) -> std::io::Result<()> {
--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,125 @@
 // main.rs
 use crate::log::LogWriter;
 use std::collections::HashMap;
-use std::fs::OpenOptions;
+use std::fs::{self, OpenOptions};
 use std::io::{stdin, ErrorKind, Read, Seek};
 use std::os::windows::fs::FileExt;
 
 pub mod log;
-// pub mod store;
 
-// HashMap<Key bytes , (value offset , value length)>
-struct Index(HashMap<Vec<u8>, (u64, usize)>);
+struct Index {
+    // HashMap<KEY BYTES ,
+    // (VALUE_OFFSET IN CURRENT SEGMENT , VALUE SIZE IN CURRENT SEGMENT , SEGMENT IN WHICH VALUE IS PRESENT)>
+    map: HashMap<Vec<u8>, (u64, usize, u32)>,
+    segment_id: u32,
+}
 
 impl Index {
     pub fn new() -> Self {
-        Index(HashMap::new())
+        Index {
+            map: HashMap::new(),
+            segment_id: 0,
+        }
     }
-    pub fn insert(&mut self, key: Vec<u8>, value_offset: u64, value_len: usize) {
-        self.0.insert(key, (value_offset, value_len));
+    pub fn insert(
+        &mut self,
+        key: Vec<u8>,
+        value_offset: u64,
+        value_len: usize,
+        segment_number: u32,
+    ) {
+        self.map
+            .insert(key, (value_offset, value_len, segment_number));
     }
-    pub fn get(&self, key: &[u8]) -> Option<&(u64, usize)> {
-        self.0.get(key)
+    pub fn get(&self, key: &[u8]) -> Option<&(u64, usize, u32)> {
+        self.map.get(key)
+    }
+
+    pub fn set_segment_id(&mut self) {
+        self.segment_id = self.get_highest_segment_id();
+    }
+    pub fn get_segment_id(&self) -> u32 {
+        self.segment_id
+    }
+    pub fn increment_segment_id(&mut self) {
+        self.segment_id += 1
+    }
+
+    fn get_highest_segment_id(&self) -> u32 {
+        let current_dir = match fs::read_dir(".") {
+            Ok(dir) => dir,
+            Err(_) => return 0,
+        };
+
+        let mut max_number = 0;
+
+        for entry in current_dir {
+            if let Ok(entry) = entry {
+                if let Some(filename) = entry.file_name().to_str() {
+                    if filename.starts_with(BASE_LOG_NAME) && filename.ends_with(".log") {
+                        let number_part = &filename[BASE_LOG_NAME.len()..filename.len() - 4];
+                        if let Ok(number) = number_part.parse::<u32>() {
+                            max_number = max_number.max(number);
+                        }
+                    }
+                }
+            }
+        }
+        max_number
     }
 }
 
+pub const BASE_LOG_NAME: &str = "basicdb";
+
 fn main() -> std::io::Result<()> {
-    let log_path = "kestrel.log";
     let mut index = Index::new();
+    index.set_segment_id();
 
-    let mut file = OpenOptions::new()
-        .create(true)
-        .write(true)
-        .read(true)
-        .open(log_path)?;
-    loop {
-        let key_len = match read_u32(&mut file) {
-            Ok(n) => n as usize,
-            Err(e) if e.kind() == ErrorKind::UnexpectedEof => {
-                eprintln!("Incomplete or partial record entry at EOF; discarding");
-                break;
-            }
+    for seg_id in 0..=index.get_segment_id() {
+        let log_path = get_log_path(&seg_id);
+        let mut file = match OpenOptions::new().read(true).open(log_path) {
+            Ok(f) => f,
+            Err(e) if e.kind() == ErrorKind::NotFound => break,
             Err(e) => return Err(e),
         };
-        let val_len = match read_u32(&mut file) {
-            Ok(n) => n as usize,
-            Err(e) if e.kind() == ErrorKind::UnexpectedEof => {
-                eprintln!("Incomplete or partial record entry in log; discarding");
-                break;
-            }
-            Err(e) => return Err(e),
-        };
-        let key = match read_bytes(&mut file, key_len)? {
-            Some(data) => data,
-            None => {
-                eprintln!("Incomplete or partial record entry in log; discarding");
-                break;
-            }
-        };
+        loop {
+            let key_len = match read_u32(&mut file) {
+                Ok(n) => n as usize,
+                Err(e) if e.kind() == ErrorKind::UnexpectedEof => {
+                    // eprintln!("Incomplete or partial record entry at EOF; discarding");
+                    break;
+                }
+                Err(e) => return Err(e),
+            };
+            let val_len = match read_u32(&mut file) {
+                Ok(n) => n as usize,
+                Err(e) if e.kind() == ErrorKind::UnexpectedEof => {
+                    eprintln!("Incomplete or partial record entry in log; discarding");
+                    break;
+                }
+                Err(e) => return Err(e),
+            };
+            let key = match read_bytes(&mut file, key_len)? {
+                Some(data) => data,
+                None => {
+                    eprintln!("Incomplete or partial record entry in log; discarding");
+                    break;
+                }
+            };
 
-        let value_offset = file.stream_position()?;
-        index.insert(key, value_offset, val_len);
+            let value_offset = file.stream_position()?;
+            // moves the cursor to end of value bytes so next read doesnt
+            // start form the middle of value bytes
+            file.seek(std::io::SeekFrom::Current(val_len as i64))?;
+            index.insert(key, value_offset, val_len, seg_id);
+        }
     }
+    println!("{:?} INDEX FILLED MAP", index.map);
 
-    let mut log_writer = LogWriter::new(log_path)?;
+    let log_path = get_log_path(&index.segment_id);
+    let mut log_writer = LogWriter::new(&log_path)?;
     log_writer.set_batch_size(1);
+
     loop {
         let mut input = String::new();
         stdin().read_line(&mut input)?;
@@ -78,17 +135,31 @@ fn main() -> std::io::Result<()> {
                 let key = parts.next().unwrap_or_default().to_string();
                 let value = parts.next().unwrap_or_default().to_string();
 
-                let (value_offset, value_len) =
-                    log_writer.write_entry(&key.clone().into_bytes(), &value.into_bytes())?;
+                let (value_offset, value_len, writer_updated) = log_writer.write_entry(
+                    &key.clone().into_bytes(),
+                    &value.into_bytes(),
+                    &index.get_segment_id(),
+                )?;
 
-                index.insert(key.clone().into_bytes(), value_offset, value_len);
+                if writer_updated {
+                    index.increment_segment_id();
+                }
+
+                index.insert(
+                    key.clone().into_bytes(),
+                    value_offset,
+                    value_len,
+                    index.get_segment_id(),
+                );
                 log_writer.maybe_sync()?;
             }
             Some("GET") => {
                 let key = parts.next().unwrap_or_default().to_string();
                 match index.get(&key.clone().into_bytes()) {
-                    Some((val_offset, val_len)) => {
-                        let _file = OpenOptions::new().read(true).open(log_path)?;
+                    Some((val_offset, val_len, segment_number)) => {
+                        let _file = OpenOptions::new()
+                            .read(true)
+                            .open(get_log_path(segment_number))?;
                         let mut buf = vec![0u8; *val_len];
                         _file.seek_read(&mut buf, *val_offset)?;
                         println!("{:?}", buf);
@@ -122,4 +193,8 @@ fn read_bytes(reader: &mut impl Read, len: usize) -> std::io::Result<Option<Vec<
         Err(e) if e.kind() == ErrorKind::UnexpectedEof => Ok(None),
         Err(e) => Err(e),
     }
+}
+
+pub fn get_log_path(segment_number: &u32) -> String {
+    format!("{}{}.log", BASE_LOG_NAME, segment_number)
 }
